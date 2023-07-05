9f483147c79eca8a9f4be9dcf19b079b325b70c6
omar faruk~ <avirus170@gmail.com>
2023-07-05 21:38:24 +0600

    added router-dom and setup tailwind

---
+++ src/routes/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../page/Home";
+import NotFound from "../page/NotFound";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Home/>,
+    },
+    {
+      path: "*",
+      element:<NotFound/>,
+    },
+])
+
+export default router
